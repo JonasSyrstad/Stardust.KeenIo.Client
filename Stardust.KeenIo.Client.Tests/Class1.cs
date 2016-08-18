fe33c9a2cada30b6671a4c538fa55307be938744
Jonas Syrstad <jonassyrstad@outlook.com>
2016-08-18 13:16:56 +0200

    initial load

--- Stardust.KeenIo.Client.Tests/Class1.cs
+++ Stardust.KeenIo.Client.Tests/Class1.cs
@@ -18,7 +18,7 @@ namespace Stardust.KeenIo.Client.Tests
             var client = ProxyFactory.CreateInstance<IEventCollector>("https://api.keen.io");
             client.SetGlobalProperty("host", Environment.MachineName).SetGlobalProperty("user",Environment.UserName);
 
-            await client.AddEvent("559d215690e4bd1556caa590", "test", new { TimeStamp = DateTime.UtcNow, Name = "UnitTest" });
+            await client.AddEvent("560c2d6e672e6c1204fba8d5", "test", new { TimeStamp = DateTime.UtcNow, Name = "UnitTest" });
 
         }
     }
